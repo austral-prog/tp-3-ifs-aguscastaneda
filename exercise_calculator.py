21db1f301205d0451970a23fac45d76e1bcb82cc
aguscastaneda <castaneda.agustin.et21.21@gmail.com>
2026-03-27 10:50:35 -0300

    intento 1

--- exercise_calculator.py
+++ exercise_calculator.py
@@ -22,4 +22,20 @@ def calculator():
         Para las entradas "10", "5" y "x", la salida esperada es:
         Operacion invalida
     """
-    pass
+    n1 = float(input())
+    n2 = float(input())
+    opreacion = input()
+
+    if opreacion == "+":
+        print("Resultado:", n1 + n2)
+    elif opreacion == "-":
+        print("Resultado:", n1 - n2)
+    elif opreacion == "*":
+        print("Resultado:", n1 * n2)
+    elif opreacion == "/" and (n1 == 0 or n2 == 0):
+        print("Error: division por cero")
+    elif opreacion == "/" and n1 != 0 and n2 != 0:
+        print("Resultado:", n1 / n2)
+    else:
+        print("Operacion invalida")
+    
